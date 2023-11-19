8a41f607af500c77ab82f9466eb257db881791d6
NateOlson17 <nate.8olson@gmail.com>
2023-11-19 16:27:13 -0700

    implemented some I2C communication

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "mbed.h"
-#include "MCP23017.h"
 
 DigitalOut MUX_0(p21);
 DigitalOut MUX_1(p22);
@@ -12,38 +11,62 @@ DigitalIn INT_2(p25);
 DigitalIn INT_3(p26);
 AnalogIn FADER_IN(p20);
 
-MCP23017 i2c(p28, p27, 0x20);
+I2C bus(p28, p27);
 
+/*
+Takes device address, register to read, buffer to store value in.
+Returns 0 on success, 1 on ACK fail.
+*/
+int readReg(uint8_t addr, uint8_t reg, uint8_t *buf) {
+    uint8_t opcode = (addr << 1) | 0x40; //format opcode as 0100AAA0, last bit writes
+    uint8_t regbyte = (reg << 1) | 0x01; //leftshift and add read bit
+    bus.start();
+    if (bus.write(opcode) == 1) { //if control byte ack by slave, send next
+        bus.start();
+        bus.write(regbyte) == 1; //send next control byte
+        *buf = bus.read(1); //read reg into buf and send ack
+        bus.stop();
+        return 0;
+    } else return 1;
+}
 
 int main()
 {
     printf("================\n");
     FADER_IN.set_reference_voltage(5);
-    while (true) {
-        if (INT_0) {
-            printf("INTERRUPT ON CHIP 0\n");
-        }
 
-        if (INT_1) {
-            printf("INTERRUPT ON CHIP 1\n");
-        }
+    bus.frequency(400000);
 
-        if (INT_2) {
-            printf("INTERRUPT ON CHIP 2\n");
-        }
+    uint8_t readval;
+    readReg(0x00, 0x00, &readval);
+    printf("%x", readval);
+    return 1;
 
-        if (INT_3) {
-            printf("INTERRUPT ON CHIP 3\n");
-        }
+    // while (true) {
+    //     if (INT_0) {
+    //         printf("INTERRUPT ON CHIP 0\n");
+    //     }
+
+    //     if (INT_1) {
+    //         printf("INTERRUPT ON CHIP 1\n");
+    //     }
+
+    //     if (INT_2) {
+    //         printf("INTERRUPT ON CHIP 2\n");
+    //     }
+
+    //     if (INT_3) {
+    //         printf("INTERRUPT ON CHIP 3\n");
+    //     }
 
 
-        printf("\n\n----------------\n\n");
-        for (uint8_t mux_addr = 0x00; mux_addr < 0x0f; mux_addr++) {
-            MUX_0 = mux_addr & 0x01;
-            MUX_1 = mux_addr & 0x02;
-            MUX_2 = mux_addr & 0x04;
-            MUX_3 = mux_addr & 0x08;
-            printf("A: %x, V: %.4f\n", mux_addr, FADER_IN.read_voltage());
-        }
-    }
+    //     printf("\n\n----------------\n\n");
+    //     for (uint8_t mux_addr = 0x00; mux_addr < 0x0f; mux_addr++) {
+    //         MUX_0 = mux_addr & 0x01;
+    //         MUX_1 = mux_addr & 0x02;
+    //         MUX_2 = mux_addr & 0x04;
+    //         MUX_3 = mux_addr & 0x08;
+    //         printf("A: %x, V: %.4f\n", mux_addr, FADER_IN.read_voltage());
+    //     }
+    // }
 }
