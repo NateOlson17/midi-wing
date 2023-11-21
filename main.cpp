cb572f0d8f29f9c9ef2f9d8c8419fd4b75cb62ab
NateOlson17 <nate.8olson@gmail.com>
2023-11-20 22:21:45 -0700

    finished R/W functionality over I2C, implemented init for slave config

--- main.cpp
+++ main.cpp
@@ -5,68 +5,98 @@ DigitalOut MUX_1(p22);
 DigitalOut MUX_2(p23);
 DigitalOut MUX_3(p24);
 
-DigitalIn INT_0(p13);
-DigitalIn INT_1(p14);
-DigitalIn INT_2(p25);
-DigitalIn INT_3(p26);
+DigitalIn INT_0A(p13);
+DigitalIn INT_1A(p14);
+DigitalIn INT_2A(p25);
+DigitalIn INT_3A(p26);
 AnalogIn FADER_IN(p20);
 
 I2C bus(p28, p27);
 
+
+//WIRE INPUTS FOR B SIDE INTERRUPTS AT 11/12/30/29 (maybe move them around for clean wiring)
+//INSTEAD OF WRITEMULTIPLE FUNC, ALLOW REGULAR WRITE FUNC TO ACCEPT 2 BYTES AND TAKE ADVANTAGE OF SEQUENTIAL OPERATION TO WRITE TO BOTH SIDES A/B (make a "both sides" bool parameter? do it by default if 2 bytes sent? bool param is best way, only requires if statement to resend byte) - is it worth it? I will only do this during init....
+
 /*
 Takes device address, register to read, buffer to store value in.
-Returns 0 on success, 1 on ACK fail.
+Returns 0 on success, -n on ACK fail, where nth sent byte was NACK by slave.
 */
 int readReg(uint8_t addr, uint8_t reg, uint8_t *buf) {
     uint8_t opcode = (addr << 1) | 0x40; //format opcode as 0100AAA0, last bit writes
-    uint8_t regbyte = (reg << 1) | 0x01; //leftshift and add read bit
     bus.start();
-    if (bus.write(opcode) == 1) { //if control byte ack by slave, send next
-        bus.start();
-        bus.write(regbyte) == 1; //send next control byte
-        *buf = bus.read(1); //read reg into buf and send ack
-        bus.stop();
-        return 0;
-    } else return 1;
+    if (bus.write(opcode) == 1) { //if control byte ack by slave, send reg byte (2 on timeout, 0 on nack)
+        if (bus.write(reg) == 1) { //if register address ack, restart and send next control byte
+            bus.start();
+            if (bus.write(opcode | 1) == 1) {//send second control byte (opcode with read bit high)
+                *buf = bus.read(1); //read reg into buf and send ack
+                bus.stop();
+                return 0;
+            } else {bus.stop(); return -3;}
+        } else {bus.stop(); return -2;}
+    } else {bus.stop(); return -1;}
+}
+
+/*
+Takes device address, register to write to, and write data buffer.
+Returns 0 on success, -n on ACK fail, where nth sent byte was NACK by slave.
+*/
+int writeReg(uint8_t addr, uint8_t reg, uint8_t *buf) {
+    uint8_t opcode = (addr << 1) | 0x40; //format opcode as 0100AAA0, last bit writes
+    bus.start();
+    if (bus.write(opcode) == 1) { //if control byte ack by slave, send reg byte (2 on timeout, 0 on nack)
+        if (bus.write(reg) == 1) { //if register address ack, send write data
+            if (bus.write(*buf) == 1) {
+                bus.stop();
+                return 0;
+            } else {bus.stop(); return -3;}
+        } else {bus.stop(); return -2;}
+    } else {bus.stop(); return -1;}
+}
+
+
+/*
+Configures:
+-All pins as inputs
+-Pin logic state to match input
+-All pins enabled for interrupt on change
+-Interrupt comparison to previous value, not DEFVAL
+-Bank addressing
+-Non mirrored interrupt pins
+-Automatic pointer incrementation
+-Interrupts as active LOW
+-Pull up resistors enabled on all pins (will idle high)
+Returns 0 on success, -1 on R/W fail.
+*/
+int busInit() {
+    bus.frequency(400000);
+    uint8_t configbuf = 0xff;
+    for (uint8_t addr = 0; addr < 4; addr++) { //write config to each slave
+        if (writeReg(addr, 0x04, &configbuf) || //GPINTENA reg
+        writeReg(addr, 0x05, &configbuf) ||
+        writeReg(addr, 0x0C, &configbuf) || //GPPUA reg
+        writeReg(addr, 0x0D, &configbuf)) {
+            return -1; //if any write fails
+        }
+    }
+    return 0;
 }
 
 int main()
 {
     printf("================\n");
     FADER_IN.set_reference_voltage(5);
+    busInit();
+    
+    while (true) {
+        //check interrupt pins
 
-    bus.frequency(400000);
-
-    uint8_t readval;
-    readReg(0x00, 0x00, &readval);
-    printf("%x", readval);
-    return 1;
-
-    // while (true) {
-    //     if (INT_0) {
-    //         printf("INTERRUPT ON CHIP 0\n");
-    //     }
-
-    //     if (INT_1) {
-    //         printf("INTERRUPT ON CHIP 1\n");
-    //     }
-
-    //     if (INT_2) {
-    //         printf("INTERRUPT ON CHIP 2\n");
-    //     }
-
-    //     if (INT_3) {
-    //         printf("INTERRUPT ON CHIP 3\n");
-    //     }
-
-
-    //     printf("\n\n----------------\n\n");
-    //     for (uint8_t mux_addr = 0x00; mux_addr < 0x0f; mux_addr++) {
-    //         MUX_0 = mux_addr & 0x01;
-    //         MUX_1 = mux_addr & 0x02;
-    //         MUX_2 = mux_addr & 0x04;
-    //         MUX_3 = mux_addr & 0x08;
-    //         printf("A: %x, V: %.4f\n", mux_addr, FADER_IN.read_voltage());
-    //     }
-    // }
+        printf("\n\n----------------\n\n");
+        for (uint8_t mux_addr = 0x00; mux_addr < 0x0f; mux_addr++) {
+            MUX_0 = mux_addr & 0x01;
+            MUX_1 = mux_addr & 0x02;
+            MUX_2 = mux_addr & 0x04;
+            MUX_3 = mux_addr & 0x08;
+            printf("A: %x, V: %.4f\n", mux_addr, FADER_IN.read_voltage());
+        }
+    }
 }
