5fa47c4f939d2a4bd383e0c2fb5e313f98b60a10
NateOlson17 <nate.8olson@gmail.com>
2024-03-04 23:55:39 -0700

    Changed MCP23017 interrupt pins to mirrored and actively driving, renamed DigitalIn interrupt detectors, improved failure reporting for busInit(), rewrote MCP23017 config on correct registers

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ DigitalOut MUX_1(p22);
 DigitalOut MUX_2(p23);
 DigitalOut MUX_3(p24);
 
-DigitalIn INT_0A(p13);
-DigitalIn INT_1A(p14);
-DigitalIn INT_2A(p25);
-DigitalIn INT_3A(p26);
+DigitalIn INT_0(p13);
+DigitalIn INT_1(p14);
+DigitalIn INT_2(p25);
+DigitalIn INT_3(p26);
+
 AnalogIn FADER_IN(p20, 3.3);
 
 I2C bus(p28, p27);
@@ -18,8 +19,14 @@ I2C bus(p28, p27);
 USBMIDI midicon;
 
 
-//FIND BETTER WAY TO DETECT FADER VAL CHANGE
-//WIRE INPUTS FOR B SIDE INTERRUPTS AT 11/12/30/29 (maybe move them around for clean wiring)
+//!Redesign curved parts and get new copy of keyshelf printed
+//!Move first 5 buttons to proper inputs
+//!Figure out why INT is constantly active (INTCON register is always 0x0A? Check what INTCONA/B is before and after IOCON write and GPPU/GPINTEN write. Checking INTF might give more info.)
+//Set up battery harness for LEDs
+//Implement sendMSC properly
+//Figure out better fader change detection
+
+
 
 /*
 Takes device address, register to read, buffer to store value in.
@@ -68,21 +75,27 @@ Configures:
 -All pins enabled for interrupt on change
 -Interrupt comparison to previous value, not DEFVAL
 -Bank addressing
--Non mirrored interrupt pins
+-Internally linked interrupt pins
 -Automatic pointer incrementation
+-INT as active driver output
 -Interrupts as active LOW
 -Pull up resistors enabled on all pins (will idle high)
 Returns 0 on success, -1 on write fail.
 */
 int busInit() {
     bus.frequency(400000);
-    uint8_t configbuf = 0xff;
+    uint8_t IOCON_cfg = 0xC0;
+    uint8_t FF_cfg = 0xff;
     for (uint8_t addr = 0; addr < 4; addr++) { //write config to each I2C slave
-        if (writeReg(addr, 0x04, &configbuf) || //GPINTENA reg
-        writeReg(addr, 0x05, &configbuf) ||
-        writeReg(addr, 0x0C, &configbuf) || //GPPUA reg
-        writeReg(addr, 0x0D, &configbuf)) {
-            return -1; //if any write fails
+        //printf("Writing config to slave %i\n", addr);
+        if (writeReg(addr, 0x0A, &IOCON_cfg)) { //IOCON reg, IOCON.BANK at POR is 0, initial IOCON reg is 0x0A, changes to 0x05 after IOCON cfg 
+            printf("IOCON WRITE FAILED\n");
+            return -1; //if write fails
+        } else if (writeReg(addr, 0x06, &FF_cfg) || //GPPUA reg
+                    writeReg(addr, 0x02, &FF_cfg) || //GPINTENA reg
+                    writeReg(addr, 0x16, &FF_cfg) || //GPPUB reg
+                    writeReg(addr, 0x12, &FF_cfg)) { //GPINTENB reg
+            printf("GPPU/GPINTEN WRITE FAILED\n");
         }
     }
     return 0;
@@ -110,11 +123,16 @@ int sendMSC(uint8_t command, uint8_t *data, int datalen) {
 int main()
 {
     printf("================\n");
-    busInit();
+    if (busInit()) {
+        printf("Bus init failed!\n");
+    }
+    // uint8_t rbuf;
+    // readReg(0x00, 0x04, &rbuf);
+    // printf("INTCONA=%x\n", rbuf);
     double faderval[15] = { }; //init fader values at 0
-    
-    while (true) {
-        for (uint8_t mux_addr = 0; mux_addr < 15; mux_addr++) { //iterate over fader address (0-15)
+
+    while (true) {        
+        for (uint8_t mux_addr = 0; mux_addr < 15; ++mux_addr) { //iterate over fader address (0-15)
             MUX_0 = mux_addr & 0x01; //send on A0-A3 pins, set mux switch to given address
             MUX_1 = mux_addr & 0x02;
             MUX_2 = mux_addr & 0x04;
@@ -124,9 +142,9 @@ int main()
                 faderval[mux_addr] = newfaderval;
                 uint8_t fader_coarse = newfaderval * 1.28; //get discrete 8 bit values
                 uint8_t fader_fine = (newfaderval - trunc(newfaderval)) * 128;
-                //printf("[A] %i [C] %i [F] %i\n", mux_addr, fader_coarse, fader_fine);
-                uint8_t mscmsg[] = {mux_addr, 1, fader_fine, fader_coarse}; //fader num, page num, fine, coarse
-                sendMSC(0x06, mscmsg, 4);
+                //printf("[A] %x [C] %i [F] %i\n", mux_addr, fader_coarse, fader_fine);
+                //uint8_t mscmsg[] = {mux_addr, 1, fader_fine, fader_coarse}; //fader num, page num, fine, coarse
+                //sendMSC(0x06, mscmsg, 4);
             }
         }
     }
