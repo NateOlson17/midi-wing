159deb55085746208adb02d87b4332d3c7c00620
NateOlson17 <nate.8olson@gmail.com>
2024-03-05 23:27:03 -0700

    Improved debugging framework, reduced register rewrite issue to reproducible example.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include "USBMIDI.h"
 #include "usbcore.h"
 
+#define DEBUG 1 //ENABLE/DISABLE DEBUG OVER SERIAL - PRINTF IS ALIASED, WILL BLOCK UNTIL COM PORT IS AVAILABLE. TURN OFF DEBUG WHEN RUNNING WITHOUT COM!
+#define VERBOSE 1 //Disabling debug will override verbose option
+
+#if DEBUG 
+  #define debug(a) printf a
+#else
+  #define debug(a) (void)0
+#endif
+
 DigitalOut MUX_0(p21);
 DigitalOut MUX_1(p22);
 DigitalOut MUX_2(p23);
@@ -80,22 +89,28 @@ Configures:
 -INT as active driver output
 -Interrupts as active LOW
 -Pull up resistors enabled on all pins (will idle high)
-Returns 0 on success, -1 on write fail.
+Returns 0 on success, -n on write fail. n = 1 for IOCON, 2 for GPPU/GPINTEN, 3 for INTCON.
 */
 int busInit() {
     bus.frequency(400000);
     uint8_t IOCON_cfg = 0xC0;
     uint8_t FF_cfg = 0xff;
+    uint8_t INTCON_cfg = 0x00;
     for (uint8_t addr = 0; addr < 4; addr++) { //write config to each I2C slave
-        //printf("Writing config to slave %i\n", addr);
-        if (writeReg(addr, 0x0A, &IOCON_cfg)) { //IOCON reg, IOCON.BANK at POR is 0, initial IOCON reg is 0x0A, changes to 0x05 after IOCON cfg 
-            printf("IOCON WRITE FAILED\n");
-            return -1; //if write fails
+        debug(("Writing config to slave %i\n", addr));
+        if (writeReg(addr, 0x0A, &IOCON_cfg)) { //IOCON reg, IOCON.BANK at POR is 0, initial IOCON reg is 0x0A, moves to 0x05 after IOCON cfg 
+            debug(("IOCON write failed!\n"));
+            return -1;
         } else if (writeReg(addr, 0x06, &FF_cfg) || //GPPUA reg
-                    writeReg(addr, 0x02, &FF_cfg) || //GPINTENA reg
                     writeReg(addr, 0x16, &FF_cfg) || //GPPUB reg
+                    writeReg(addr, 0x02, &FF_cfg) || //GPINTENA reg
                     writeReg(addr, 0x12, &FF_cfg)) { //GPINTENB reg
-            printf("GPPU/GPINTEN WRITE FAILED\n");
+            debug(("GPPU/GPINTEN write failed!\n"));
+            return -2;
+        } else if (writeReg(addr, 0x04, &INTCON_cfg) || //INTCONA reg
+                    writeReg(addr, 0x14, &INTCON_cfg)) { //INTCONB reg
+            debug(("INTCON write failed!\n"));
+            return -3;
         }
     }
     return 0;
@@ -120,32 +135,61 @@ int sendMSC(uint8_t command, uint8_t *data, int datalen) {
     return 0;
 }
 
+void reportRegisterStatus() {
+    uint8_t rbuf;
+    readReg(0x00, 0x02, &rbuf);
+    debug(("GPINTENA=%x | ", rbuf));
+    readReg(0x00, 0x12, &rbuf);
+    debug(("GPINTENB=%x | ", rbuf));
+    readReg(0x00, 0x04, &rbuf);
+    debug(("INTCONA=%x | ", rbuf));
+    readReg(0x00, 0x14, &rbuf);
+    debug(("INTCONB=%x | ", rbuf));
+    readReg(0x00, 0x05, &rbuf);
+    debug(("IOCON=%x | ", rbuf));
+    readReg(0x00, 0x15, &rbuf);
+    debug(("IOCON=%x | ", rbuf));
+    readReg(0x00, 0x06, &rbuf);
+    debug(("GPPUA=%x | ", rbuf));
+    readReg(0x00, 0x16, &rbuf);
+    debug(("GPPUB=%x | ", rbuf));
+    readReg(0x00, 0x09, &rbuf);
+    debug(("GPIOA=%x | ", rbuf));
+    readReg(0x00, 0x19, &rbuf);
+    debug(("GPIOB=%x | ", rbuf));
+    if (INT_0) {
+        debug(("INT HIGH\n"));
+    } else {
+        debug(("INT LOW\n"));
+    }
+}
+
 int main()
 {
-    printf("================\n");
+    debug(("========MAIN========\n"));
     if (busInit()) {
-        printf("Bus init failed!\n");
+        debug(("I2C bus init failed!\n"));
+    } else {
+        debug(("I2C bus init success!\n"));
     }
-    // uint8_t rbuf;
-    // readReg(0x00, 0x04, &rbuf);
-    // printf("INTCONA=%x\n", rbuf);
-    double faderval[15] = { }; //init fader values at 0
 
-    while (true) {        
-        for (uint8_t mux_addr = 0; mux_addr < 15; ++mux_addr) { //iterate over fader address (0-15)
-            MUX_0 = mux_addr & 0x01; //send on A0-A3 pins, set mux switch to given address
-            MUX_1 = mux_addr & 0x02;
-            MUX_2 = mux_addr & 0x04;
-            MUX_3 = mux_addr & 0x08;
-            double newfaderval = FADER_IN.read() * 100;
-            if (abs(newfaderval - faderval[mux_addr]) > .3) { //if current val differs from prev by more than .3%, update
-                faderval[mux_addr] = newfaderval;
-                uint8_t fader_coarse = newfaderval * 1.28; //get discrete 8 bit values
-                uint8_t fader_fine = (newfaderval - trunc(newfaderval)) * 128;
-                //printf("[A] %x [C] %i [F] %i\n", mux_addr, fader_coarse, fader_fine);
-                //uint8_t mscmsg[] = {mux_addr, 1, fader_fine, fader_coarse}; //fader num, page num, fine, coarse
-                //sendMSC(0x06, mscmsg, 4);
-            }
-        }
+    //double faderval[15] = { }; //init fader values at 0
+    while (true) {
+        if (VERBOSE) {reportRegisterStatus();}
+        // for (uint8_t mux_addr = 0; mux_addr < 15; ++mux_addr) { //iterate over fader address (0-15)
+        //     MUX_0 = mux_addr & 0x01; //send on A0-A3 pins, set mux switch to given address
+        //     MUX_1 = mux_addr & 0x02;
+        //     MUX_2 = mux_addr & 0x04;
+        //     MUX_3 = mux_addr & 0x08;
+        //     double newfaderval = FADER_IN.read() * 100;
+        //     if (abs(newfaderval - faderval[mux_addr]) > .3) { //if current val differs from prev by more than .3%, update
+        //         faderval[mux_addr] = newfaderval;
+        //         uint8_t fader_coarse = newfaderval * 1.28; //get discrete 8 bit values
+        //         uint8_t fader_fine = (newfaderval - trunc(newfaderval)) * 128;
+        //         //printf("[A] %x [C] %i [F] %i\n", mux_addr, fader_coarse, fader_fine);
+        //         //uint8_t mscmsg[] = {mux_addr, 1, fader_fine, fader_coarse}; //fader num, page num, fine, coarse
+        //         //sendMSC(0x06, mscmsg, 4);
+        //     }
+        // }
     }
 }
